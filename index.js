b5f344388adc7b3c76ba191f91905c909fe43dc9
PrayForPleasures <dmitriyyarchuck@gmail.com>
2023-10-13 01:16:13 +0300

    scnd comm

---
+++ index.js
@@ -0,0 +1,11 @@
+const scrollbox = document.querySelector(
+    ".section__last-box_scroll-box_mini-box-one"
+);
+const scrollbar = document.querySelector(".scrollbar");
+const scroller = document.querySelector(".scroller");
+
+const heightOfScrollBar = scrollbar.scrollHeight;
+
+function addScrollBarMove() {
+    console.log(scrollbox.childElementCount);
+}
