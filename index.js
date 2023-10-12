b1d896917b6c16df59605d84e0d4407adff46c40
PrayForPleasures <dmitriyyarchuck@gmail.com>
2023-10-13 01:38:52 +0300

    scnd comm

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const scrollbar = document.querySelector(".scrollbar");
 const scroller = document.querySelector(".scroller");
 
 const heightOfScrollBar = scrollbar.scrollHeight;
+const heightOfScroller = heightOfScrollBar / scrollbox.childElementCount;
+scroller.setAttribute("style", `height: ${heightOfScroller}px`);
 
 function addScrollBarMove() {
-    console.log(scrollbox.childElementCount);
+    console.log(heightOfScroller);
 }
